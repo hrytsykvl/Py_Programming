29255fa10c4a90a121df8e542c0a894139001c51
Vlad Hrytsyk <115397164+hrytsykvl@users.noreply.github.com>
2023-11-11 14:58:29 +0200

    Modify delete method and add method to search workers by query

--- Workers/Worker.py
+++ Workers/Worker.py
@@ -31,8 +31,11 @@ class WorkerDB:
     def add_worker(self, worker):
         self.workers.append(worker)
 
-    def delete_worker(self, worker):
-        self.workers.remove(worker)
+    def delete_worker(self, local_id):
+        for i in range(len(self.workers)):
+            if self.workers[i].get_id() == local_id:
+                self.workers.pop(i)
+                return
 
     def read_workers(self, filename):
         with open(filename, 'r') as file:
@@ -51,6 +54,15 @@ class WorkerDB:
 
     def sort_workers(self, field, ascending=True):
         self.workers.sort(key=lambda worker: getattr(worker, field), reverse=not ascending)
+
+    def search_workers(self, search_query):
+        search_results = []
+        for worker in self.workers:
+            if search_query.lower() in str(worker).lower():
+                search_results.append(worker)
+        print("Search Results:\n")
+        for ad in search_results:
+            print(ad, end="\n\n")
 
     def display_workers(self):
         for worker in self.workers:
