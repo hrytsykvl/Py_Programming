00b4e11bfb0396a8fbf75c9b613401523b625999
Vlad Hrytsyk <115397164+hrytsykvl@users.noreply.github.com>
2023-11-11 15:37:24 +0200

    Make menu and split classes and menu into different files

--- Workers/Worker.py
+++ Workers/Worker.py
@@ -1,5 +1,3 @@
-import csv
-
 id_count = 0
 
 
@@ -22,48 +20,3 @@ class Worker:
                 f"Surname: {self.surname}\n"
                 f"Department: {self.department}\n"
                 f"Salary: {self.salary}\n")
-
-
-class WorkerDB:
-    def __init__(self):
-        self.workers = []
-
-    def add_worker(self, worker):
-        self.workers.append(worker)
-
-    def delete_worker(self, local_id):
-        for i in range(len(self.workers)):
-            if self.workers[i].get_id() == local_id:
-                self.workers.pop(i)
-                return
-
-    def read_workers(self, filename):
-        with open(filename, 'r') as file:
-            reader = csv.reader(file)
-            next(reader)
-            for row in reader:
-                name, surname, department, salary = row
-                worker = Worker(name, surname, department, salary)
-                self.add_worker(worker)
-
-    def edit_workers(self, local_id, local_label, edited_label):
-        for worker in self.workers:
-            if worker.get_id() == local_id:
-                worker_to_edit = worker
-                setattr(worker_to_edit, local_label, edited_label)
-
-    def sort_workers(self, field, ascending=True):
-        self.workers.sort(key=lambda worker: getattr(worker, field), reverse=not ascending)
-
-    def search_workers(self, search_query):
-        search_results = []
-        for worker in self.workers:
-            if search_query.lower() in str(worker).lower():
-                search_results.append(worker)
-        print("Search Results:\n")
-        for ad in search_results:
-            print(ad, end="\n\n")
-
-    def display_workers(self):
-        for worker in self.workers:
-            print(worker)
---
+++ Workers/main.py
@@ -0,0 +1,62 @@
+from WorkerDB import WorkerDB
+from Worker import Worker
+
+MENU = """
+1. Load data from csv file
+2. Add worker
+3. Delete worker
+4. Sort workers
+5. Search workers
+6. Edit worker
+7. Show workers
+0. Quit
+"""
+
+
+def main():
+    workers = WorkerDB()
+
+    while True:
+        print(MENU)
+        choice = input("Enter your choice: ")
+
+        match choice:
+            case '1':
+                try:
+                    filepath = input("Enter file you want to read from: ")
+                    workers.read_workers(filepath)
+                except Exception as ex:
+                    print(ex)
+            case '2':
+                name = input("Enter name: ")
+                surname = input("Enter surname: ")
+                department = input("Enter department: ")
+                salary = float(input("Enter salary: "))
+                worker = Worker(name, surname, department, salary)
+                workers.add_worker(worker)
+            case '3':
+                worker_id = int(input("Enter id: "))
+                workers.delete_worker(worker_id)
+            case '4':
+                field_name = input("Enter field name to sort(name, surname, department, salary): ")
+                ascending = input("Sort in ascending order? (y/n): ").lower().startswith('y')
+                workers.sort_workers(field_name, ascending)
+            case '5':
+                search_query = input("Enter search query: ")
+                workers.search_workers(search_query)
+            case '6':
+                worker_id = int(input("Enter id: "))
+                field_to_edit = input("Enter field name to edit(name, surname, department, salary): ")
+                edited_field = input(f"Enter the new value for {field_to_edit}:")
+                workers.edit_workers(worker_id, field_to_edit, edited_field)
+            case '7':
+                workers.display_workers()
+            case '0':
+                return
+            case _:
+                print("Invalid choice. Please enter a valid option.")
+                continue
+
+
+if __name__ == "__main__":
+    main()
